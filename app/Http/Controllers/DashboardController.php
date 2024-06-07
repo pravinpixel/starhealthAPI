b6f1ecff89623c4d3e8bef3c75f2c5967245649a
manikandan <139951024+Manikandan211@users.noreply.github.com>
2024-06-07 11:53:12 +0530

    dasboard changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-         return view('dashboard');
+        $sub_mission = Employee::count();
+        $final_list = Employee::where('employee_status','final')->count();
+        $completed = Employee::where('status','completed')->count();
+        $in_completed = Employee::whereIn('status',['basic','upload','summary'])->count();
+        $shortlist = Employee::where('employee_status','shortlist')->count();
+
+         return view('dashboard',[
+            'in_completed'=>$in_completed,
+            'sub_mission'=>$sub_mission,
+            'completed'=>$completed,
+            'final_list'=> $final_list,
+            'shortlist'=> $shortlist,
+
+         ]);
     }
 
 }
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -46,7 +46,7 @@
      <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">{{$title}} List</h1>
      <!--end::Title-->
      <!--begin::Breadcrumb-->
-     {{-- <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+     <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
       <!--begin::Item-->
       <li class="breadcrumb-item text-muted">
        <a href="{{url('dashboard')}}"  class="text-muted text-hover-primary">Home</a>
@@ -58,17 +58,9 @@
       </li>
       <!--end::Item-->
       <!--begin::Item-->
-      <li class="breadcrumb-item text-muted">Masters</li>
+      <li class="breadcrumb-item text-muted">{{$title}} List</li>
       <!--end::Item-->
-      <!--begin::Item-->
-      <li class="breadcrumb-item">
-       <span class="bullet bg-gray-400 w-5px h-2px"></span>
-      </li>
-      <!--end::Item-->
-      <!--begin::Item-->
-      <li class="breadcrumb-item text-muted">Year</li>
-      <!--end::Item-->
-     </ul> --}}
+     </ul>
      <!--end::Breadcrumb-->
     </div>
     <!--end::Page title-->
