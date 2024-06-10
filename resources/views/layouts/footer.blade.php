b2488fad94bc57bbc85e0f5c21f92bc8e99fb025
manikandan <139951024+Manikandan211@users.noreply.github.com>
2024-06-10 14:46:19 +0530

    changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
             class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
             <div class="text-dark order-2 order-md-1">
                 <span class="text-muted fw-semibold me-1">{{ date('Y')}}&copy;</span>
-                <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">StartHelath</a>
+                <a href="#" target="_blank" class="text-gray-800 text-hover-primary">StartHelath</a>
             </div>
             <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
             </ul>
