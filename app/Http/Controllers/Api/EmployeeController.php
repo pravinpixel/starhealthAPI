79b2b69294867f48d433f5a2a5d3dec809b37053
manikandan <139951024+Manikandan211@users.noreply.github.com>
2024-06-13 12:40:29 +0530

    changes

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -31,11 +31,11 @@ class EmployeeController extends Controller
         try {
            
             $validator = Validator::make($request->all(), [
-                // 'email' => [
-                //     'required',
-                //     'email',
-                //     'regex:/^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(starhealth|starinsurance)\.in$|^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@pixel-studios\.com$/'
-                // ],
+                'email' => [
+                    'required',
+                    'email',
+                    'regex:/^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(starhealth|starinsurance)\.in$|^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@pixel-studios\.com$/'
+                ],
                 'token' => 'required'
             ]); 
             if($validator->fails()) {
