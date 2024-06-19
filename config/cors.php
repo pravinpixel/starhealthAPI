d60009bb44697621286a34e48245958cbeb29969
manikandan <139951024+Manikandan211@users.noreply.github.com>
2024-06-19 12:55:42 +0530

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -17,13 +17,13 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET, POST, PUT, DELETE, OPTIONS'],
 
     'allowed_origins' => ['http://127.0.0.1:4000'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type, Authorization'],
 
     'exposed_headers' => [],
 
