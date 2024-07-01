53052a40547770e396d2c5f235e255ba0d41d8d6
manikandan <139951024+Manikandan211@users.noreply.github.com>
2024-07-01 17:24:54 +0530

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,10 +11,10 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
         $sub_mission = Employee::count();
-        $final_list = Employee::where('employee_status','final')->count();
+        $final_list = Employee::where('employee_status','final')->where('status','completed')->count();
         $completed = Employee::where('status','completed')->whereNotNull('profile_photo')->whereNotNull('passport_photo')->count();
         $in_completed = Employee::whereIn('status',['basic','upload','summary'])->count();
-        $shortlist = Employee::where('employee_status','shortlist')->whereNotNull('profile_photo')->whereNotNull('passport_photo')->count();
+        $shortlist = Employee::where('employee_status','shortlist')->where('status','completed')->whereNotNull('profile_photo')->whereNotNull('passport_photo')->count();
         $today_sub_mission = Employee::whereDate('created_at', Carbon::today())->count();
         $today_completed = Employee::whereDate('created_at', Carbon::today())->where('status','completed')->count();
         $today_final_list = Employee::whereDate('created_at', Carbon::today())->where('employee_status','final')->count();
