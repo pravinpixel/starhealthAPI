5fd72f2b6f756a359c40e5965d209fb79cc11ac3
manikandan <139951024+Manikandan211@users.noreply.github.com>
2024-07-24 18:43:44 +0530

    changes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -59,6 +59,32 @@
               </div>
               @endif
               <div class="menu-item">
+                <a class="menu-link {{(request()->is('employee/register-list*')) ? 'active' : '' }}" href="{{url('employee/register-list')}}">
+                      <span class="menu-icon">
+                        <span class="svg-icon svg-icon-2">
+                          <svg width="800px" height="800px" viewBox="-3 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  
+                            <title>fileboard_checklist [#1595]</title>
+                            <desc>Created with Sketch.</desc>
+                            <defs>
+                        
+                        </defs>
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Dribbble-Light-Preview" transform="translate(-303.000000, -1919.000000)" fill="#000000">
+                                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                                        <path d="M258,1774 L258,1774 C258,1774.552 257.552,1775 257,1775 L254,1775 C253.448,1775 253,1774.552 253,1774 C253,1773.448 253.448,1773 254,1773 L257,1773 C257.552,1773 258,1773.448 258,1774 L258,1774 Z M258,1771 L258,1771 C258,1771.552 257.552,1772 257,1772 L254,1772 C253.448,1772 253,1771.552 253,1771 C253,1770.448 253.448,1770 254,1770 L257,1770 C257.552,1770 258,1770.448 258,1771 L258,1771 Z M258,1768 C258,1768.552 257.552,1769 257,1769 L254,1769 C253.448,1769 253,1768.552 253,1768 C253,1767.448 253.448,1767 254,1767 L257,1767 C257.552,1767 258,1767.448 258,1768 L258,1768 Z M252,1774 L252,1774 C252,1774.552 251.552,1775 251,1775 C250.448,1775 250,1774.552 250,1774 C250,1773.448 250.448,1773 251,1773 C251.552,1773 252,1773.448 252,1774 L252,1774 Z M252,1771 L252,1771 C252,1771.552 251.552,1772 251,1772 C250.448,1772 250,1771.552 250,1771 C250,1770.448 250.448,1770 251,1770 C251.552,1770 252,1770.448 252,1771 L252,1771 Z M252,1768 C252,1768.552 251.552,1769 251,1769 C250.448,1769 250,1768.552 250,1768 C250,1767.448 250.448,1767 251,1767 C251.552,1767 252,1767.448 252,1768 L252,1768 Z M259,1776 C259,1776.552 258.552,1777 258,1777 L250,1777 C249.448,1777 249,1776.552 249,1776 L249,1763.5 C249,1763.224 249.224,1763 249.5,1763 L250,1763 C250,1764.104 250.895,1765 252,1765 L256,1765 C257.105,1765 258,1764.104 258,1763 L258.5,1763 C258.776,1763 259,1763.224 259,1763.5 L259,1776 Z M252,1762 C252,1761.448 252.448,1761 253,1761 L255,1761 C255.552,1761 256,1761.448 256,1762 C256,1762.552 255.552,1763 255,1763 L253,1763 C252.448,1763 252,1762.552 252,1762 L252,1762 Z M259,1761 L258,1761 C258,1759.895 257.105,1759 256,1759 L252,1759 C250.895,1759 250,1759.895 250,1761 L249,1761 C247.895,1761 247,1761.895 247,1763 L247,1777 C247,1778.104 247.895,1779 249,1779 L259,1779 C260.105,1779 261,1778.104 261,1777 L261,1763 C261,1761.895 260.105,1761 259,1761 L259,1761 Z" id="fileboard_checklist-[#1595]">
+                        
+                        </path>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
+                        </span>
+                      </span>
+                      <span  style="color: white" class="menu-title">Submitted</span>
+                    </a>
+            </div>
+              <div class="menu-item">
                 <a class="menu-link {{(request()->is('employee/submitted-gallery*')) ? 'active' : '' }}" href="{{url('employee/submitted-gallery')}}">
                       <span class="menu-icon">
                         <span class="svg-icon svg-icon-2">
@@ -85,6 +111,32 @@
                     </a>
          </div>
          <div class="menu-item">
+          <a class="menu-link {{(request()->is('employee/short-list*')) ? 'active' : '' }}" href="{{url('employee/short-list')}}">
+                <span class="menu-icon">
+                  <span class="svg-icon svg-icon-2">
+                    <svg width="800px" height="800px" viewBox="-3 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+                      <title>fileboard_done [#1584]</title>
+                      <desc>Created with Sketch.</desc>
+                      <defs>
+                  
+                  </defs>
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="Dribbble-Light-Preview" transform="translate(-343.000000, -1959.000000)" fill="#000000">
+                              <g id="icons" transform="translate(56.000000, 160.000000)">
+                                  <path d="M299,1803.5 C299,1803.224 298.776,1803 298.5,1803 L298,1803 C298,1804.104 297.105,1805 296,1805 L292,1805 C290.895,1805 290,1804.104 290,1803 L289.5,1803 C289.224,1803 289,1803.224 289,1803.5 L289,1816 C289,1816.552 289.448,1817 290,1817 L298,1817 C298.552,1817 299,1816.552 299,1816 L299,1803.5 Z M292,1802 C292,1802.552 292.448,1803 293,1803 L295,1803 C295.552,1803 296,1802.552 296,1802 C296,1801.448 295.552,1801 295,1801 L293,1801 C292.448,1801 292,1801.448 292,1802 L292,1802 Z M301,1803 L301,1817 C301,1818.104 300.105,1819 299,1819 L289,1819 C287.895,1819 287,1818.104 287,1817 L287,1803 C287,1801.895 287.895,1801 289,1801 L290,1801 C290,1799.895 290.895,1799 292,1799 L296,1799 C297.105,1799 298,1799.895 298,1801 L299,1801 C300.105,1801 301,1801.895 301,1803 L301,1803 Z M296.828,1807.879 C297.219,1808.269 297.219,1808.902 296.828,1809.293 L293.293,1812.828 C291.549,1811.084 291.907,1811.442 291.172,1810.707 C290.781,1810.317 290.781,1809.683 291.172,1809.293 C291.562,1808.902 292.195,1808.902 292.586,1809.293 L293.293,1810 L295.414,1807.879 C295.805,1807.488 296.438,1807.488 296.828,1807.879 L296.828,1807.879 Z" id="fileboard_done-[#1584]">
+                  
+                  </path>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                  </span>
+                </span>
+                <span  style="color: white" class="menu-title">Shortlisted</span>
+              </a>
+      </div>
+         <div class="menu-item">
           <a class="menu-link {{(request()->is('employee/shortlist-gallery*')) ? 'active' : '' }}" href="{{url('employee/shortlist-gallery')}}">
             <span class="menu-icon">
               <span class="svg-icon svg-icon-2">
@@ -110,8 +162,38 @@
                 <span  style="color: white" class="menu-title">Shortlisted  Gallery View</span>
               </a>
    </div>
+     
+   <div class="menu-item">
+    <a class="menu-link {{(request()->is('employee/final-list*')) ? 'active' : '' }}" href="{{url('employee/final-list')}}">
+          <span class="menu-icon">
+            <span class="svg-icon svg-icon-2">
+              <svg width="800px" height="800px" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+                <title>done [#1476]</title>
+                <desc>Created with Sketch.</desc>
+                <defs>
+            
+            </defs>
+                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g id="Dribbble-Light-Preview" transform="translate(-219.000000, -400.000000)" fill="#000000">
+                        <g id="icons" transform="translate(56.000000, 160.000000)">
+                            <path d="M181.9,258 L165.1,258 L165.1,242 L173.5,242 L173.5,240 L163,240 L163,260 L184,260 L184,250 L181.9,250 L181.9,258 Z M170.58205,245.121 L173.86015,248.243 L182.5153,240 L184,241.414 L173.86015,251.071 L173.86015,251.071 L173.8591,251.071 L169.09735,246.536 L170.58205,245.121 Z" id="done-[#1476]">
+            
+            </path>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+            </span>
+          </span>
+          <span  style="color: white" class="menu-title">Finalist</span>
+        </a>
+   </div>
+    <div>        
+    </div>
+  </div>
       <div class="menu-item">
-        <a class="menu-link {{(request()->is('employee/finallist-gallery*')) ? 'active' : '' }}" href="{{url('employee/finallist-gallery')}}">
+        <a style="margin-left: 12px;" class="menu-link {{(request()->is('employee/finallist-gallery*')) ? 'active' : '' }}" href="{{url('employee/finallist-gallery')}}">
               <span class="menu-icon">
                 <span class="svg-icon svg-icon-2">
                   <svg width="800px" height="800px" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -136,87 +218,8 @@
               <span  style="color: white" class="menu-title">Finalist  Gallery View</span>
             </a>
     </div>
-                <div class="menu-item">
-                  <a class="menu-link {{(request()->is('employee/register-list*')) ? 'active' : '' }}" href="{{url('employee/register-list')}}">
-                        <span class="menu-icon">
-                          <span class="svg-icon svg-icon-2">
-                            <svg width="800px" height="800px" viewBox="-3 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    
-                              <title>fileboard_checklist [#1595]</title>
-                              <desc>Created with Sketch.</desc>
-                              <defs>
-                          
-                          </defs>
-                              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                  <g id="Dribbble-Light-Preview" transform="translate(-303.000000, -1919.000000)" fill="#000000">
-                                      <g id="icons" transform="translate(56.000000, 160.000000)">
-                                          <path d="M258,1774 L258,1774 C258,1774.552 257.552,1775 257,1775 L254,1775 C253.448,1775 253,1774.552 253,1774 C253,1773.448 253.448,1773 254,1773 L257,1773 C257.552,1773 258,1773.448 258,1774 L258,1774 Z M258,1771 L258,1771 C258,1771.552 257.552,1772 257,1772 L254,1772 C253.448,1772 253,1771.552 253,1771 C253,1770.448 253.448,1770 254,1770 L257,1770 C257.552,1770 258,1770.448 258,1771 L258,1771 Z M258,1768 C258,1768.552 257.552,1769 257,1769 L254,1769 C253.448,1769 253,1768.552 253,1768 C253,1767.448 253.448,1767 254,1767 L257,1767 C257.552,1767 258,1767.448 258,1768 L258,1768 Z M252,1774 L252,1774 C252,1774.552 251.552,1775 251,1775 C250.448,1775 250,1774.552 250,1774 C250,1773.448 250.448,1773 251,1773 C251.552,1773 252,1773.448 252,1774 L252,1774 Z M252,1771 L252,1771 C252,1771.552 251.552,1772 251,1772 C250.448,1772 250,1771.552 250,1771 C250,1770.448 250.448,1770 251,1770 C251.552,1770 252,1770.448 252,1771 L252,1771 Z M252,1768 C252,1768.552 251.552,1769 251,1769 C250.448,1769 250,1768.552 250,1768 C250,1767.448 250.448,1767 251,1767 C251.552,1767 252,1767.448 252,1768 L252,1768 Z M259,1776 C259,1776.552 258.552,1777 258,1777 L250,1777 C249.448,1777 249,1776.552 249,1776 L249,1763.5 C249,1763.224 249.224,1763 249.5,1763 L250,1763 C250,1764.104 250.895,1765 252,1765 L256,1765 C257.105,1765 258,1764.104 258,1763 L258.5,1763 C258.776,1763 259,1763.224 259,1763.5 L259,1776 Z M252,1762 C252,1761.448 252.448,1761 253,1761 L255,1761 C255.552,1761 256,1761.448 256,1762 C256,1762.552 255.552,1763 255,1763 L253,1763 C252.448,1763 252,1762.552 252,1762 L252,1762 Z M259,1761 L258,1761 C258,1759.895 257.105,1759 256,1759 L252,1759 C250.895,1759 250,1759.895 250,1761 L249,1761 C247.895,1761 247,1761.895 247,1763 L247,1777 C247,1778.104 247.895,1779 249,1779 L259,1779 C260.105,1779 261,1778.104 261,1777 L261,1763 C261,1761.895 260.105,1761 259,1761 L259,1761 Z" id="fileboard_checklist-[#1595]">
-                          
-                          </path>
-                                      </g>
-                                  </g>
-                              </g>
-                          </svg>
-                          </span>
-                        </span>
-                        <span  style="color: white" class="menu-title">Submitted</span>
-                      </a>
-              </div>
-              <div class="menu-item">
-                <a class="menu-link {{(request()->is('employee/short-list*')) ? 'active' : '' }}" href="{{url('employee/short-list')}}">
-                      <span class="menu-icon">
-                        <span class="svg-icon svg-icon-2">
-                          <svg width="800px" height="800px" viewBox="-3 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    
-                            <title>fileboard_done [#1584]</title>
-                            <desc>Created with Sketch.</desc>
-                            <defs>
-                        
-                        </defs>
-                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <g id="Dribbble-Light-Preview" transform="translate(-343.000000, -1959.000000)" fill="#000000">
-                                    <g id="icons" transform="translate(56.000000, 160.000000)">
-                                        <path d="M299,1803.5 C299,1803.224 298.776,1803 298.5,1803 L298,1803 C298,1804.104 297.105,1805 296,1805 L292,1805 C290.895,1805 290,1804.104 290,1803 L289.5,1803 C289.224,1803 289,1803.224 289,1803.5 L289,1816 C289,1816.552 289.448,1817 290,1817 L298,1817 C298.552,1817 299,1816.552 299,1816 L299,1803.5 Z M292,1802 C292,1802.552 292.448,1803 293,1803 L295,1803 C295.552,1803 296,1802.552 296,1802 C296,1801.448 295.552,1801 295,1801 L293,1801 C292.448,1801 292,1801.448 292,1802 L292,1802 Z M301,1803 L301,1817 C301,1818.104 300.105,1819 299,1819 L289,1819 C287.895,1819 287,1818.104 287,1817 L287,1803 C287,1801.895 287.895,1801 289,1801 L290,1801 C290,1799.895 290.895,1799 292,1799 L296,1799 C297.105,1799 298,1799.895 298,1801 L299,1801 C300.105,1801 301,1801.895 301,1803 L301,1803 Z M296.828,1807.879 C297.219,1808.269 297.219,1808.902 296.828,1809.293 L293.293,1812.828 C291.549,1811.084 291.907,1811.442 291.172,1810.707 C290.781,1810.317 290.781,1809.683 291.172,1809.293 C291.562,1808.902 292.195,1808.902 292.586,1809.293 L293.293,1810 L295.414,1807.879 C295.805,1807.488 296.438,1807.488 296.828,1807.879 L296.828,1807.879 Z" id="fileboard_done-[#1584]">
-                        
-                        </path>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                        </span>
-                      </span>
-                      <span  style="color: white" class="menu-title">Shortlisted</span>
-                    </a>
-            </div>
-            <div class="menu-item">
-              <a class="menu-link {{(request()->is('employee/final-list*')) ? 'active' : '' }}" href="{{url('employee/final-list')}}">
-                    <span class="menu-icon">
-                      <span class="svg-icon svg-icon-2">
-                        <svg width="800px" height="800px" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    
-                          <title>done [#1476]</title>
-                          <desc>Created with Sketch.</desc>
-                          <defs>
-                      
-                      </defs>
-                          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                              <g id="Dribbble-Light-Preview" transform="translate(-219.000000, -400.000000)" fill="#000000">
-                                  <g id="icons" transform="translate(56.000000, 160.000000)">
-                                      <path d="M181.9,258 L165.1,258 L165.1,242 L173.5,242 L173.5,240 L163,240 L163,260 L184,260 L184,250 L181.9,250 L181.9,258 Z M170.58205,245.121 L173.86015,248.243 L182.5153,240 L184,241.414 L173.86015,251.071 L173.86015,251.071 L173.8591,251.071 L169.09735,246.536 L170.58205,245.121 Z" id="done-[#1476]">
-                      
-                      </path>
-                                  </g>
-                              </g>
-                          </g>
-                      </svg>
-                      </span>
-                    </span>
-                    <span  style="color: white" class="menu-title">Finalist</span>
-                  </a>
-             </div>
-              <div>        
-              </div>
-            </div>
+               
+            
         </div>
   </div>
 </div>
